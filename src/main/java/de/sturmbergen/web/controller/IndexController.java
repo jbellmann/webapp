530c7b982b6b8a5a0ca446fabd53af4cbc919037
blackanthrax <blackanthrax666@googlemail.com>
2014-11-03 18:25:40 +0100

    status service

--- src/main/java/de/sturmbergen/web/controller/IndexController.java
+++ src/main/java/de/sturmbergen/web/controller/IndexController.java
@@ -1,13 +1,26 @@
 package de.sturmbergen.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.sturmbergen.service.status.ServerStatus;
+import de.sturmbergen.service.status.ServerStatusService;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private ServerStatusService statusService;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(@ModelAttribute ServerStatus status, Model model) {
+
+	ServerStatus status2 = statusService.getStatus();
+
+	model.addAttribute(status2);
 	return "index";
     }
 }
